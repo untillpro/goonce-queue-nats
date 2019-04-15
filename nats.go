67a3ced3d575fb55d222e1dc2be472287d81e356
kvv <v.kuslin@untill.com>
2019-04-15 16:51:17 +0300

    work with subscribers

--- nats.go
+++ nats.go
@@ -5,14 +5,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/nats-io/go-nats"
+	"github.com/untillpro/gochips"
 	"github.com/untillpro/godif"
 	"github.com/untillpro/igoonce/iconfig"
 	"github.com/untillpro/igoonce/iqueues"
 	"hash/fnv"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -27,6 +28,7 @@ func Declare() {
 
 var partitionHandlerFactories map[string]iqueues.PartitionHandlerFactory
 var nonPartyHandlers map[string]iqueues.NonPartyHandler
+var partitionHandlers map[string]iqueues.PartitionHandler
 
 type NatsConfig struct {
 	Servers string
@@ -37,11 +39,69 @@ type natsWorker struct {
 	conn *nats.Conn
 }
 
+type natsSubscriber struct {
+	queueID           string
+	partitionDividend int
+	numOfPartition    int
+	partitionsNumber  int
+	worker            *natsWorker
+}
+
 type natsQueueKey int
 
 const (
-	natsConnection natsQueueKey = 0
+	natsPublish   natsQueueKey = 0
+	natsSubscribe natsQueueKey = 1
 )
+
+func connectPublisher(conf *NatsConfig) (worker *natsWorker, err error) {
+	return connectToNats(conf, "NatsPublisher")
+}
+
+func connectSubscribers(conf *NatsConfig) (subscribers []*natsSubscriber, err error) {
+	subscribers = make([]*natsSubscriber, 0)
+	for k := range nonPartyHandlers {
+		queueAndPartition := strings.Split(k, ":")
+		queueName := queueAndPartition[0]
+		worker, err := connectToNats(conf, queueName)
+		if err != nil {
+			return nil, err
+		}
+		subscriber := &natsSubscriber{k, 0, 0, 0, worker}
+		subscribers = append(subscribers, subscriber)
+	}
+	for k := range partitionHandlerFactories {
+		queueAndPartition := strings.Split(k, ":")
+		queueName := queueAndPartition[0]
+		numOfPartitions, err := strconv.Atoi(queueAndPartition[1])
+		if err != nil {
+			return nil, err
+		}
+		partitionDividend, _, err := calculatePartitionNumber(k, numOfPartitions)
+		if err != nil {
+			return nil, err
+		}
+		for i := 0; i < numOfPartitions; i++ {
+			worker, err := connectToNats(conf, fmt.Sprintf("%s_%d", queueName, i))
+			if err != nil {
+				return nil, err
+			}
+			subscribers = append(subscribers, &natsSubscriber{k, partitionDividend, i, numOfPartitions, worker})
+		}
+	}
+	return subscribers, nil
+}
+
+func connectToNats(conf *NatsConfig, connectionName string) (worker *natsWorker, err error) {
+	opts := setupConnOptions([]nats.Option{nats.Name(connectionName)})
+	opts = setupConnOptions(opts)
+	natsConn, err := nats.Connect(conf.Servers, opts...)
+	if err != nil {
+		return nil, err
+	}
+	worker = &natsWorker{conf, natsConn}
+	return worker, nil
+}
 
 func Init(ctx context.Context) (context.Context, error) {
 	var config NatsConfig
@@ -49,13 +109,97 @@ func Init(ctx context.Context) (context.Context, error) {
 	if err != nil {
 		return nil, err
 	}
-	opts := setupConnOptions([]nats.Option{})
-	natsConn, err := nats.Connect(config.Servers, opts...)
+	workers, err := connectSubscribers(&config)
 	if err != nil {
+		//TODO don't forget to release resources
 		return nil, err
 	}
-	worker := natsWorker{&config, natsConn}
-	return context.WithValue(ctx, natsConnection, worker), nil
+	ctx = context.WithValue(ctx, natsSubscribe, workers)
+	worker, err := connectPublisher(&config)
+	if err != nil {
+		//TODO don't forget to release resources
+		return nil, err
+	}
+	return context.WithValue(ctx, natsPublish, worker), nil
+}
+
+func (v *natsSubscriber) createNatsHandler(handler interface{}) nats.MsgHandler {
+	nc := v.worker.conn
+	return func(msg *nats.Msg) {
+		req := iqueues.Request{
+			QueueID:           msg.Subject,
+			PartitionNumber:   v.numOfPartition,
+			PartitionDividend: v.partitionDividend,
+			Args:              msg.Data,
+		}
+		resp := handler(ctx, &req)
+		data, err := json.Marshal(resp)
+		if err != nil {
+			//do smth with error
+			log.Fatal(err)
+		}
+		nc.Publish(msg.Reply, data)
+	}
+}
+
+func Start(ctx context.Context) {
+	subscribers := ctx.Value(natsSubscribe).([]*natsSubscriber)
+	for _, v := range subscribers {
+		var natsHandler nats.MsgHandler
+		if v.partitionsNumber == 0 {
+			nc := v.worker.conn
+			handler := nonPartyHandlers[v.queueID]
+			natsHandler = func(msg *nats.Msg) {
+				req := iqueues.Request{
+					QueueID: msg.Subject,
+					Args:    msg.Data,
+				}
+				resp := handler(ctx, &req)
+				data, err := json.Marshal(resp)
+				if err != nil {
+					//do smth with error
+					log.Fatal(err)
+				}
+				nc.Publish(msg.Reply, data)
+			}
+		} else {
+			if val, ok := partitionHandlers[fmt.Sprintf("%s_%d", v.queueID, v.numOfPartition)]; ok {
+				val.Handle(ctx)
+			} else {
+
+			}
+			handlerFactory := v.queueID
+			natsHandler = func(msg *nats.Msg) {
+				req := iqueues.Request{
+					QueueID: msg.Subject,
+					Args:    msg.Data,
+				}
+				resp := handlerFactory(ctx)
+				data, err := json.Marshal(resp)
+				if err != nil {
+					//do smth with error
+					log.Fatal(err)
+				}
+				worker.conn.Publish(msg.Reply, data)
+			}
+		}
+	}
+}
+
+func (w *natsWorker) subscribe(handler nats.MsgHandler) error {
+	conn := w.conn
+	_, err := conn.Subscribe(conn.Opts.Name, handler)
+	err = conn.Flush()
+	if err = conn.LastError(); err != nil {
+		//TODO no need to stop all, just one subscribtion
+		return err
+	}
+	return nil
+}
+
+//TODO unsubscribe
+func Stop(ctx context.Context) {
+
 }
 
 //TODO close connection to nats
@@ -64,22 +208,24 @@ func Finit(ctx context.Context) error {
 	return nil
 }
 
-//TODO recreate http.Request to our Request with Args=*http.Request or pass partitionKey to method or read mux.Vars() (gorilla-mux specific)
-// or parse URL here (bad) or pass map[string]string with params from router (not library-specific), but we need to know key names
 func invokeFromHTTPRequest(ctx context.Context, request *iqueues.Request, response http.ResponseWriter, timeout time.Duration) {
-	worker := ctx.Value(natsConnection).(natsWorker)
-	HTTPRequest := request.Args.(*http.Request)
-	body, err := ioutil.ReadAll(HTTPRequest.Body)
-	if err != nil {
-		http.Error(response, "can't read request body: "+string(body), http.StatusBadRequest)
-		return
+	worker := ctx.Value(natsPublish).(natsWorker)
+	HTTPRequestBody := request.Args.([]byte)
+	queueNameAndPartitionNumber := strings.Split(request.QueueID, ":")
+	if len(queueNameAndPartitionNumber) == 0 {
+		http.Error(response, "wrong QueueID", http.StatusBadRequest)
 	}
+	numOfPartitions, err := strconv.Atoi(queueNameAndPartitionNumber[1])
+	if err != nil {
+		http.Error(response, "wrong QueueID", http.StatusBadRequest)
+	}
+	gochips.Info(fmt.Sprintf("Queue name: %s; number of partitions: %d", queueNameAndPartitionNumber[0], numOfPartitions))
 	var resp *iqueues.Response
-	if request.PartitionDividend == 0 {
-		resp = worker.reqRespNats(body, request.QueueID, timeout)
+	if numOfPartitions == 0 {
+		resp = worker.reqRespNats(HTTPRequestBody, request.QueueID, timeout)
 	} else {
-		request.PartitionNumber, err = calculatePartitionNumber(request.QueueID, request.PartitionDividend)
-		resp = worker.reqRespNats(body, request.QueueID+strconv.Itoa(request.PartitionNumber), timeout)
+		request.PartitionNumber, request.PartitionDividend, err = calculatePartitionNumber(request.QueueID, numOfPartitions)
+		resp = worker.reqRespNats(HTTPRequestBody, queueNameAndPartitionNumber[0]+strconv.Itoa(request.PartitionNumber), timeout)
 	}
 	data := resp.Data.([]byte)
 	_, err = fmt.Fprint(response, data)
@@ -112,30 +258,12 @@ func (w *natsWorker) reqRespNats(data interface{}, partitionKey string, timeout 
 	return createResponse(http.StatusOK, msg.Data)
 }
 
-func InitConnection(ctx context.Context, partitionName string) *nats.Conn {
-
-}
-
-//TODO no need to return *Response and pass *Request
-// need to create one conn for each handler and reuse it
-// we need Handler interface and structs with *nats.Conn
-var airBoView iqueues.NonPartyHandler = func(ctx context.Context, queueID string, request *iqueues.Request) *iqueues.Response {
-	worker := ctx.Value(natsConnection).(natsWorker)
-	conn := worker.conn
-	_, err := conn.Subscribe(queueID, func(msg *nats.Msg) {
-		//no need to check errors, we just got timeout request in publisher
-		// maybe we need it later
-		conn.Publish(msg.Reply, []byte("airBoView"))
-	})
-	err = conn.Flush()
-
-	if err = conn.LastError(); err != nil {
-		//TODO need to log it and maybe try to reconnect
-		log.Fatal(err)
+var airBoView iqueues.NonPartyHandler = func(ctx context.Context, request *iqueues.Request) *iqueues.Response {
+	return &iqueues.Response{
+		Status:     http.StatusText(http.StatusOK),
+		StatusCode: http.StatusOK,
+		Data:       fmt.Sprintf("Received message from %s with args: %s", request.QueueID, request.Args),
 	}
-
-	log.Printf("Subscriber for subject %s created and waits for request", queueID)
-	return nil
 }
 
 //TODO here we can create new connection for PartitionHandler, try to connect, if success return handler, if failure
@@ -143,17 +271,17 @@ var airBoView iqueues.NonPartyHandler = func(ctx context.Context, queueID string
 // i should start connections here? Handler structs for partitionDivident num?
 var factory iqueues.PartitionHandlerFactory = func(ctx context.Context, queueID string, partitionDividend int,
 	partitionNumber int) (handler *iqueues.PartitionHandler, err *iqueues.Response) {
-
 	return nil, &iqueues.Response{}
 }
 
-func calculatePartitionNumber(queueID string, partitionDividend int) (int, error) {
+func calculatePartitionNumber(queueID string, numOfPartitions int) (partitionDividend int, partitionNumber int, err error) {
 	h := fnv.New32a()
-	_, err := h.Write([]byte(queueID))
+	_, err = h.Write([]byte(queueID))
 	if err != nil {
-		return 0, err
+		return 0, 0, err
 	}
-	return int(h.Sum32()) % partitionDividend, nil
+	partitionDividend = int(h.Sum32())
+	return partitionDividend, partitionDividend % numOfPartitions, nil
 }
 
 func createResponse(HTTPStatusCode int, data interface{}) *iqueues.Response {
